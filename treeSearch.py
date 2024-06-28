650b6cabae524e024421f8468416f31722822009
Martin Konečný <xkonecm00@fit.vutbr.cz>
2024-06-28 18:14:35 +0200

    predict_on_batch

--- treeSearch.py
+++ treeSearch.py
@@ -1,18 +1,31 @@
 import random
-
 import chess
 import numpy as np
 import Functions
 import keras
 from chess import pgn
-model = keras.saving.load_model('firstModel.keras')
+import time
+import tensorflow as tf
 
+model = keras.models.load_model('firstModel.keras')
+
+def randomPlay(board):
+    legals = list(board.legal_moves)
+    return legals[random.randint(0, len(legals)-1)]
 
 def eval(fen):
-    #bitboard = Functions.fen_to_bitboards(fen)
-    #bitboard = bitboard.reshape(1, 15, 8, 8)
-    #return model.predict(bitboard, verbose=0)
-    return random.randint(-1000, 1000)
+    bitboards = Functions.fen_to_bitboards(fen)
+    bitboards = bitboards.reshape(1, 15, 8, 8)
+    #start = time.time()
+    #predict = model(bitboards)
+    #end = time.time()
+    #print("model(x)", end - start, predict)
+    #start = time.time()
+    predict = model.predict_on_batch(bitboards)
+    #end = time.time()
+    #print("batch_predict", end - start, predict)
+    return predict
+    #return random.randint(-1000, 1000)
 
 def alphaBeta(board, depth, alpha, beta, maximize, move_sequence):
     if board.is_checkmate():
@@ -24,7 +37,7 @@ def alphaBeta(board, depth, alpha, beta, maximize, move_sequence):
         return eval(board.fen()), move_sequence
 
     if maximize:
-        bestVal = -99999
+        bestVal = -9999
         bestSequence = move_sequence
         legals = board.legal_moves
         for move in legals:
@@ -39,7 +52,7 @@ def alphaBeta(board, depth, alpha, beta, maximize, move_sequence):
                 return bestVal, bestSequence
         return bestVal, bestSequence
     else:
-        bestVal = 99999
+        bestVal = 9999
         bestSequence = move_sequence
         legals = board.legal_moves
         for move in legals:
@@ -54,6 +67,9 @@ def alphaBeta(board, depth, alpha, beta, maximize, move_sequence):
                 return bestVal, bestSequence
         return bestVal, bestSequence
 
+
+weights = model.layers[0].get_weights()
+
 # game settings
 game = chess.pgn.Game()
 game.headers["White"] = "White player name"
@@ -64,19 +80,18 @@ board = chess.Board()
 
 node = game
 
-for i in range(100):
-#while not board.is_game_over():
-    bestVal, bestSequence = alphaBeta(board, 3, -99999, 99999, board.turn, [])
-    board.push(bestSequence[0])
-    node = node.add_variation(bestSequence[0])
-    print(f"{bestSequence[0]}")
+
+while not board.is_game_over():
+    if board.turn == chess.WHITE:
+        move = randomPlay(board)
+        board.push(move)
+        node = node.add_variation(move)
+        print(f"{move}")
+    else:
+        bestVal, bestSequence = alphaBeta(board, 3, -99999, 99999, board.turn, [])
+        board.push(bestSequence[0])
+        node = node.add_variation(bestSequence[0])
+        print(f"{bestSequence[0]}")
 
 game.headers["Result"] = board.result()
 print(game)
-
-
-
-
-
-
-
