f8361bcc25a3540fb14f21f20ee5970b69336539
Martin Konečný <xkonecm00@fit.vutbr.cz>
2024-07-30 09:45:58 +0200

    adding first C scripts

---
+++ C_engine/treeSearch.c
@@ -0,0 +1,203 @@
+#include <stdio.h>
+#include <limits.h>
+#include <time.h>
+#include "model.c"
+
+// Structure to represent a node in the game tree
+struct Node {
+    int value;
+    // Add more fields relevant to your game
+};
+
+int numOfNodes = 0;
+
+// Function to perform alpha-beta pruning
+int alphaBetaPruning(struct Node node, int depth, int alpha, int beta, int isMaximizingPlayer) {
+
+    if (node.value == 1000){
+        //if node.turn == chess.WHITE:
+            return -10000;
+        //else:
+            //return 10000, move_sequence
+    }
+    
+    if (depth == 0 /* Add other stopping conditions here */) {
+        //evaluation
+
+        numOfNodes++;
+        return node.value; // Evaluate the node's value using your evaluation function
+    }
+
+    if (isMaximizingPlayer) {
+        int maxEval = INT_MIN;
+
+        struct Node childNode;
+        childNode.value = 20;
+        // Generate child nodes and iterate through them
+        // Update maxEval and alpha based on evaluations
+        for (int i = 0; i < 20; i++) {
+            int eval = alphaBetaPruning(childNode, depth - 1, alpha, beta, 0);
+            maxEval = eval > maxEval ? eval : maxEval;
+            alpha = alpha > eval ? alpha : eval;
+            if (beta <= alpha) {
+                break; // Beta cutoff
+            }
+        }
+        return maxEval;
+    } 
+    else 
+    {
+        int minEval = INT_MAX;
+        // Generate child nodes and iterate through them
+        // Update minEval and beta based on evaluations
+        struct Node childNode;
+        childNode.value = 20;
+        for (int i = 0; i < 20 ; i++) 
+        {
+            int eval = alphaBetaPruning(childNode, depth - 1, alpha, beta, 1);
+            minEval = eval < minEval ? eval : minEval;
+            beta = beta < eval ? beta : eval;
+            if (beta <= alpha) {
+                break; // Alpha cutoff
+            }
+        }
+        return minEval;
+    }
+}
+
+int main() {
+    
+    float bitboard[13][8][8] = {{{0, 0, 0, 0, 0, 0, 0, 0,},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {1, 1, 1, 1, 1, 1, 1, 1},
+  {0, 0, 0, 0, 0, 0, 0, 0}},
+
+ {{0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 1, 0, 0, 0, 0, 1, 0}},
+
+ {{0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 1, 0, 0, 1, 0, 0}},
+
+ {{0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {1, 0, 0, 0, 0, 0, 0, 1}},
+
+ {{0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 1, 0, 0, 0, 0}},
+
+ {{0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 1, 0, 0, 0}},
+
+ {{0, 0, 0, 0, 0, 0, 0, 0},
+  {1, 1, 1, 1, 1, 1, 1, 1},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0}},
+
+ {{0, 1, 0, 0, 0, 0, 1, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0}},
+
+ {{0, 0, 1, 0, 0, 1, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0}},
+
+ {{1, 0, 0, 0, 0, 0, 0, 1},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0}},
+
+ {{0, 0, 0, 1, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0}},
+
+ {{0, 0, 0, 0, 1, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0}},
+
+ {{1, 0, 0, 0, 0, 0, 0, 1},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {1, 1, 0, 0, 0, 0, 0, 1}}};
+
+
+    float output[1];
+    entry(&bitboard, &output);
+    printf("%f", output[0]);
+
+    // Initialize the root node and other necessary data
+    struct Node rootNode;
+    // Initialize other variables
+    time_t start = time(NULL);
+    // Call alphaBetaPruning function to find the optimal move
+    int optimalValue = alphaBetaPruning(rootNode, 12, INT_MIN, INT_MAX, 1);
+    time_t end = time(NULL);
+    
+    printf("Optimal value: %d, Number of nodes: %d, Time: %d, NodesPerSecond: %d", optimalValue, numOfNodes, (end - start), (numOfNodes / (end - start)));
+    
+    return 0;
+}
