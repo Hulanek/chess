a3dcd23c0d97f2b886848dcdcc6a0c815bccbb9a
Martin Konečný <xkonecm00@fit.vutbr.cz>
2024-08-20 09:37:17 +0200

    added info of TT

--- transpositionTable.py
+++ transpositionTable.py
@@ -2,7 +2,7 @@ import chess
 import chess.polyglot
 import sys
 
-size = 64 # has to be power of two 2^x
+size = 65536 # has to be power of two 2^x
 
 class TEntry:
     zobrist_hash = 0
@@ -21,7 +21,29 @@ class TT:
 
 
     def infoTT(self):
+        usedEntries = 0
+        numDepthZero = 0
+        numExact = 0
+        numUpper = 0
+        numLower = 0
+
+        percentageCover = 0
+
+        for i in range(len(self.TTArray)):
+            if self.TTArray[i].zobrist_hash != 0:
+                usedEntries += 1
+                if self.TTArray[i].depth == 0:
+                    numDepthZero += 1
+
+        percentageCover = usedEntries / len(self.TTArray) * 100
         print("Info about Transposition table")
+        print("Num of used Entries:", usedEntries)
+        print("Num of depth zero:", numDepthZero)
+        print("Percentage cover:", percentageCover)
+
+
+
+
     def contentOfTT(self):
         for i in range(len(self.TTArray)):
             print(self.TTArray[i].zobrist_hash, self.TTArray[i].evaluation, self.TTArray[i].best_move, self.TTArray[i].depth, self.TTArray[i].flag, self.TTArray[i].move_number)
@@ -38,25 +60,6 @@ class TT:
         self.TTArray[indexHash] = tempEntry
 
     def readEntry(self, zobrist):
-        tempEntry = TEntry()
         tempEntry = self.TTArray[zobrist & (size - 1)]
         isSameZobrist = (tempEntry.zobrist_hash == zobrist)
         return tempEntry, isSameZobrist
-
-board = chess.Board()
-legals = board.legal_moves
-zobrist_hash = chess.polyglot.zobrist_hash(board)
-evaluation = 0.321500
-for move in legals:
-    oneMove = move
-
-
-TTinstance = TT()
-TTinstance.writeEntry(zobrist_hash, evaluation, oneMove, 5, False, 6)
-Entry, isSameZobrist = TTinstance.readEntry(zobrist_hash)
-board = chess.Board()
-zobrist_hash = chess.polyglot.zobrist_hash(board)
-TTinstance.writeEntry(zobrist_hash, evaluation, oneMove, 6, False, 6)
-print(Entry.zobrist_hash, Entry.evaluation)
-print(isSameZobrist)
-print(TTinstance.contentOfTT())
